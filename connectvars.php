75a232145fb0977b5527e5e19725e8c47ffdcfc3
lingxueli <ll2871@columbia.edu>
2015-10-09 16:42:12 -0400

    Add all implementations to repository.

---
+++ connectvars.php
@@ -0,0 +1,7 @@
+<?php
+  // Define database connection constants
+  define('DB_HOST', 'localhost'); // ADD your server address here.
+  define('DB_USER', 'XXX'); // ADD your MySQL database access username here.
+  define('DB_PASSWORD', 'XXX'); // ADD your MySQL database access password here.
+  define('DB_NAME', 'mismatchdb');
+?>
